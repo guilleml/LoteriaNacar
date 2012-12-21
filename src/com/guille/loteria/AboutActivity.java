59bb046864a8bdd07f4a123c479b165d7406f77c
guilleml <guilleml@gmail.com>
2012-12-21 02:30:22 +0100

    primera subida

---
+++ src/com/guille/loteria/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.guille.loteria;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+
+
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+
+    
+  }
